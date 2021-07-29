d12e0f23b34e33960e4cd00fd588148200932d6e
Situu <jad.khachchab@gmail.com>
2021-07-29 16:13:52 +0300

    navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,29 +1,39 @@
-import React from 'react'
-import "./navbar.css"
-import { NotificationsNone, Language, Settings } from "@material-ui/icons"
+import "./Navbar.css"
+import { Search, Person, Chat, Notifications }  from "@material-ui/icons"
 
 export default function Navbar() {
     return (
-    <div className="navbar">
-        <div className="navbarWrapper">
-          <div className="navLeft">
-              <span className="logo">Arteract</span>
-          </div>
-          <div className="navRight">
-              <div className="navIcons">
-                <NotificationsNone/>
-                <span className="navIconNumber">2</span>
-              </div>
-              <div className="navIcons">
-                <Language/>
-                <span className="navIconNumber">2</span>
-              </div>
-              <div className="navIcons">
-                <Settings/>
-              </div>
-              <img src="https://cdn.discordapp.com/attachments/359402161194532869/856617609985523732/324716b51e5dc98c93ead3a74dfb409c.jpg" alt="" className="navAvatar"/>
-          </div>
+        <div className="navbarContainer">
+            <div className="navbarLeft">
+                <span className="navbarLogo">Arteract</span>
+            </div>
+            <div className="navbarCenter">
+                <div className="searchBar">
+                    <Search className="searchIcon"/>
+                    <input placeholder="Search for friend, post or video" className="searchInput"/>
+                </div>    
+            </div>
+            <div className="navbarRight">
+                <div className="navbarLinks">
+                    <span className="navbarLink">Homepage</span>
+                    <span className="navbarLink">Timeline</span>
+                </div>
+                <div className="navbarIcons">
+                    <div className="navbarIconItem">
+                        <Person/>
+                        <span className="navbarIconBadge">1</span>
+                    </div>
+                    <div className="navbarIconItem">
+                        <Chat/>
+                        <span className="navbarIconBadge">1</span>
+                    </div>
+                    <div className="navbarIconItem">
+                        <Notifications/>
+                        <span className="navbarIconBadge">1</span>
+                    </div>
+                </div>
+                <img src="" alt="" className="navbarImg"/>
+            </div>
         </div>
-    </div>
-    )
+    );
 }
