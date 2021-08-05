882ca0903de4fa9b08f0e568398ce5df833d8613
Situu <jad.khachchab@gmail.com>
2021-08-05 22:43:15 +0300

    register request

---
+++ server/models/User.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        require: true,
+        min: 2,
+        max: 20,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        max: 75,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6
+    },
+    profilePicture: {
+        type: String,
+        default: ""
+    },
+    coverPicture: {
+        type: String,
+        default: ""
+    },
+    followers: {
+        type: Array,
+        default:[]
+    },
+    followins: {
+        type: Array,
+        default:[]
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
+},
+{
+    timestamps: true
+});
+
+module.exports = mongoose.model("User", userSchema);
