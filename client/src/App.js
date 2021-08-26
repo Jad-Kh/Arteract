c9d8b27d9d72337d4767dc435141fc777b017976
Situu <70975136+Jad-Kh@users.noreply.github.com>
2021-08-26 17:49:32 +0300

    Routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,32 @@ import Home from "./pages/Home/Home";
 import Profile from "./pages/Profile/Profile";
 import CommentSection from "./pages/CommentSection/CommentSection";
 import Transactions from "./pages/Transactions/Transactions";
+import Friends from "./pages/Friends/Friends";
 import "./App.css";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Transactions />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route path="/profile">
+          <Profile/>
+        </Route>
+        <Route path="/transactions">
+          <Transactions/>
+        </Route>
+        <Route path="/comments">
+          <CommentSection/>
+        </Route>
+        <Route path="/friends">
+          <Friends/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
