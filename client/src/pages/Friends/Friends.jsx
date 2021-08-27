2fa9afc9b39b9d728f65148df9d6272be562500c
Situu <70975136+Jad-Kh@users.noreply.github.com>
2021-08-27 19:07:10 +0300

    Friends Page

--- client/src/pages/Friends/Friends.jsx
+++ client/src/pages/Friends/Friends.jsx
@@ -1,9 +1,22 @@
+import Navbar from "../../components/Navbar/Navbar"
+import Sidebar from "../../components/Sidebar/Sidebar"
+import Friendslist from "../../components/Friendslist/Friendslist"
+import Followers from "../../components/Followers/Followers"
+import Requests from "../../components/Requests/Requests"
 import "./Friends.css"
 
 export default function Friends() {
     return (
-        <div className="friends">
-            
+        <div>
+            <Navbar/>
+            <div className="friends">
+                <Sidebar/>
+                <Friendslist/>
+                <div className="friendsFollowers">
+                    <Followers/>
+                    <Requests/>
+                </div>
+            </div>
         </div>
     )
 }
