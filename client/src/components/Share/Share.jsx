5aff839aa2d3ee28bbc89bc042eb7a6ad09e3250
Situu <70975136+Jad-Kh@users.noreply.github.com>
2021-09-19 13:52:12 +0300

    File Upload

--- client/src/components/Share/Share.jsx
+++ client/src/components/Share/Share.jsx
@@ -1,10 +1,15 @@
-import { createRef, useEffect, useState } from 'react';
-import "./Share.css"
-import { PermMedia, Label, EmojiEmotions } from "@material-ui/icons"
-import Picker from "emoji-picker-react"
+import { createRef, useRef, useEffect, useState, useContext } from 'react';
+import "./Share.css";
+import { PermMedia, Label, EmojiEmotions } from "@material-ui/icons";
+import Picker from "emoji-picker-react";
+import { AuthContext } from "../../context/AuthContext";
 import axios from "axios";
 
 export default function Share() {
+
+    const { user: currentUser } = useContext(AuthContext);
+    const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+    const [file, setFile] = useState();
 
     const inputRef = createRef();
     const [message, setMessage] = useState('');
@@ -33,12 +38,12 @@ export default function Share() {
     const [ user, setUser ] = useState({});
     useEffect(() => {
         const fetchUser = async() => {
-            const response = await axios.get(`users/612f91749c6548039c771b25`);
+            const response = await axios.get("users/" + currentUser._id);
             setUser(response.data);
         }
         fetchUser();
         inputRef.current.selectionEnd = cursorPosition;
-    }, [cursorPosition])
+    }, [cursorPosition], currentUser._id)
 
     let emojiClass = hideState ? "emojiPickerContainerHidden" : "emojiPickerContainer" ;
 
@@ -46,20 +51,26 @@ export default function Share() {
         <div className="share"> 
             <div className="shareContainer">
                 <div className="shareTop">
-                    <img className="shareImg" src={"assets/avatars/" + user.profilePicture} alt="" />
+                    <img className="shareImg" src={
+                                                    user.profilePicture
+                                                    ? PF + "avatars/" + user.profilePicture
+                                                    : PF + "avatars/default.jpg"
+                                                  } alt=""/>
                     <input className="shareInput" placeholder={ "What's on your mind " + user.username + "?" }
                      value={message} onChange={handleChange} ref={inputRef}/>
                 </div>
                 <hr className="shareLine"></hr>
-                <div className="shareBottom">
+                <form className="shareBottom">
                     <div className="shareOptions">
-                        <div className="shareOptionsItem">
+                        <label htmlFor="file" className="shareOptionsItem">
                             <div class="fileUpload">
                                 <PermMedia htmlColor="lightgreen" className="shareIcon"/>
-                                <input type="file" class="upload"></input>
                             </div>
                             <span>Art Content</span>
-                        </div>
+                            <input type="file" id="file" accept=".png,.jpeg,.jpg,.gif" style={{ display: "none" }}
+                                   onChange={ (e) => setFile(e.target.files[0]) }>
+                            </input>
+                        </label>
                         <div className="shareOptionsItem">
                             <Label htmlColor="lightgreen" className="shareIcon"/>
                             <span>Tag</span>
@@ -70,7 +81,7 @@ export default function Share() {
                         </div>
                     </div>
                     <button className="shareButton">Post</button>
-                </div>
+                </form>
                 { 
                 <div className={emojiClass}>
                     <div className={`emoji-list ${!showEmojis && 'hidden'}`}>
