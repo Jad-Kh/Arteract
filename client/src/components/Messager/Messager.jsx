aff2ea61efe66072729ddd0999839a3412f95cbe
Situu <70975136+Jad-Kh@users.noreply.github.com>
2022-01-03 14:37:14 +0200

    Chat sockets real time messages

--- client/src/components/Messager/Messager.jsx
+++ client/src/components/Messager/Messager.jsx
@@ -3,12 +3,15 @@ import Message from "../Message/Message";
 import { useState, useContext, useRef, useEffect } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import axios from "axios";
+import { io } from "socket.io-client";
 
 export default function Messager({currentChat, setCurrentChat, messages, setMessages}) {
 
     const [ newMessage, setNewMessage ] = useState("");
+    const [ arrivalMessage, setArrivalMessage ] = useState();
     const scrollRef = useRef();
     const { user } = useContext(AuthContext);
+    const socket = useRef();
 
     const handleSumbit = async(e) => {
         e.preventDefault();
@@ -18,10 +21,41 @@ export default function Messager({currentChat, setCurrentChat, messages, setMess
             conversationId : currentChat._id
         }
 
+        const receiverId = currentChat.members.find(member => member !== user._id);
+
+        socket.current?.emit("sendMessage", {
+            senderId: user._id,
+            receiverId,
+            text: newMessage,
+        });
+
         const response = await axios.post("/messages", message);
         setMessages([...messages, response.data]);
         setNewMessage("");
     };
+
+    useEffect(() => {
+        socket.current = io("ws://localhost:8900");
+        socket.current.on("getMessage", (messageData) => {
+            setArrivalMessage({
+                sender: messageData.senderId,
+                text: messageData.text,
+                createdAt: Date.now(),
+            });
+        });
+    }, []);
+
+    useEffect(() => {
+        arrivalMessage &&
+        currentChat?.members.includes(arrivalMessage.sender) &&
+        setMessages((prev) => [...prev, arrivalMessage]);
+    }, [arrivalMessage, currentChat]);
+
+    useEffect(() => {
+        socket.current.emit("addUser", user._id);
+        socket.current.on("getUsers", users => {
+        });
+    }, [user]);
 
     useEffect(() => {
         scrollRef.current?.scrollIntoView({ behavior: "smooth" });
