eee28ffe142355f7c976d457fac46279e3dbb3c1
Situu <70975136+Jad-Kh@users.noreply.github.com>
2022-01-20 14:08:09 +0200

    detect online friends in activity

--- client/src/components/Social/Social.jsx
+++ client/src/components/Social/Social.jsx
@@ -1,20 +1,41 @@
 import "./Social.css"
 import { MarkunreadMailbox } from "@material-ui/icons";
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect, useContext, useRef } from "react";
 import { AuthContext } from "../../context/AuthContext"
 import axios from "axios";
+import { io } from "socket.io-client";
 
 export default function Social() {
 
     const { user } = useContext(AuthContext);
     const [ friends, setFriends ] = useState([]);
+    const socket = useRef();
+    const [ onlineUsers, setOnlineUsers ] = useState([]);
+    const [ onlineFriends, setOnlineFriends ] = useState([]);
+    const [ offlineFriends, setOfflineFriends ] = useState([]);
+
     useEffect(() => {
+        socket.current = io("ws://localhost:8900");
         const fetchFriends = async() => {
             const response = await axios.get("/users/" + user._id + "/friends");
             setFriends(response.data);
         }
         fetchFriends();
-    },[])
+    },[]);
+
+    useEffect(() => {
+        socket.current.emit("addUser", user._id);
+        socket.current.on("getUsers", users => {
+            setOnlineUsers(
+                user.friends.filter((f) => users.some((u) => u.userId === f))
+            );
+        });
+    }, [user]);
+
+    useEffect(() => {
+        setOnlineFriends(friends.filter((f) => onlineUsers.some((f2) => f2 === f._id)));
+        setOfflineFriends(friends.filter((f) => !onlineUsers.some((f2) => f2 === f._id)));
+    }, [friends, onlineUsers]);
 
     return (
         <div className="social">
@@ -25,11 +46,20 @@ export default function Social() {
                 </div>
                 <h4 className="socialFriendsTitle">Online Friends: </h4>
                 <ul className="socialFriendlist">
-                {friends.map(friend => (       
+                {onlineFriends.map(friend => (       
                     <li className="socialFriend">
                         <div className="socialFriendImgContainer">
                             <img className="socialFriendImg" src={"assets/avatar/" + friend.profilePicture} alt=""/>
                             <span className="socialFriendOnlineBadge"></span>
+                        </div>
+                        <span className="socialFriendOnlineUsername">{friend.username}</span>
+                    </li>
+                ))}
+                {offlineFriends.map(friend => (       
+                    <li className="socialFriend">
+                        <div className="socialFriendImgContainer">
+                            <img className="socialFriendImg" src={"assets/avatar/" + friend.profilePicture} alt=""/>
+                            <span className="socialFriendOfflineBadge"></span>
                         </div>
                         <span className="socialFriendOnlineUsername">{friend.username}</span>
                     </li>
