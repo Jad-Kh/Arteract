f55557f4ab9e47c102aec1cb03a0488695f1c399
Situu <70975136+Jad-Kh@users.noreply.github.com>
2022-02-14 12:34:16 +0200

    new routes and multer setup

--- server/index.js
+++ server/index.js
@@ -13,6 +13,11 @@ const userRoute = require('../server/routes/users.js');
 const postRoute = require('../server/routes/posts.js');
 const messageRoute = require('../server/routes/messages.js');
 const conversationRoute = require('../server/routes/conversations.js');
+const artworkRoute = require('../server/routes/artworks.js');
+const orderRoute = require('../server/routes/orders.js');
+const portfolioRoute = require('../server/routes/portfolios.js');
+const sectionRoute = require('../server/routes/sections.js');
+const storesRoute = require('../server/routes/stores.js');
 
 dotenv.config();
 
@@ -28,6 +33,7 @@ app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 app.use("/assets/avatars", express.static(path.join(__dirname, "public/assets/avatars")));
 app.use("/assets/backgrounds", express.static(path.join(__dirname, "public/assets/backgrounds")));
 app.use("/assets/posts/", express.static(path.join(__dirname, "public/assets/posts/"))); 
+app.use("/assets/artworks/", express.static(path.join(__dirname, "public/assets/artworks/"))); 
 // telling the API, don't make a GET request when "/assets" is requested, but just go to that directory
 app.use(express.json());
 app.use(helmet());
@@ -60,9 +66,19 @@ const postStorage = multer.diskStorage({
     }
 });
 
+const artworkStorage = multer.diskStorage({
+    destination: (request, file, cb) => {
+        cb(null, "public/assets/artworks/");
+    },
+    filename: (request, file, cb) => {
+        cb(null, (Date.now() + file.originalname).slice(0,9) + (Date.now() + file.originalname).slice(-4));
+    }
+});
+
 const avatarUpload = multer({storage: avatarStorage});
 const backgroundUpload = multer({storage: backgroundStorage});
 const postUpload = multer({storage: postStorage});
+const artworkUpload = multer({storage: artworkStorage});
 
 app.post("/api/uploadavatar", avatarUpload.single("file"), (request, response) => {
     try {
@@ -89,11 +105,24 @@ app.post("/api/uploadpost", postUpload.single("file"), (request, response) => {
     }
 });
 
+app.post("/api/uploadartwork", artworkUpload.single("file"), (request, response) => {
+    try {
+        return response.status(200).json("File uploaded successfully");
+    } catch(error) {
+        console.log(error);
+    }
+});
+
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/messages", messageRoute);
 app.use("/api/conversations", conversationRoute);
+app.use("/api/artworks", artworkRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/portfolios", portfolioRoute);
+app.use("/api/sections", sectionRoute);
+app.use("/api/stores", storesRoute);
 
 app.listen(8800, () => {
     console.log("Running Backend!");
